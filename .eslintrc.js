3fa773149c2dda75861448bff813b1e2263a4b81
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2021-10-06 07:10:55 +0200

    Changed Express.js to NestJS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,85 +1,24 @@
 module.exports = {
-  env: {
-    "node": true,
-    "browser": true,
-    "es6": true
-  },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript"
-  ],
-  globals: {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
-  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    project: 'tsconfig.json',
+    sourceType: 'module',
   },
-  plugins: [
-    "@typescript-eslint"
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  settings: {
-    "import/resolver": {
-      "node": { 
-        "paths": ["src", "src/controllers", "src/models", "src/framework", "src/services"]
-      }
-    }
+  root: true,
+  env: {
+    node: true,
+    jest: true,
   },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    "@typescript-eslint/indent": ["error", 2, { "SwitchCase": 1 }], 
-    "@typescript-eslint/interface-name-prefix": "off",
-    "max-len": ["warn", { 
-      "code": 115, 
-      "ignoreUrls": true, 
-      "ignoreStrings": true, 
-      "ignoreComments": true, 
-      "ignoreTemplateLiterals": true 
-    }],
-    "keyword-spacing": "error",
-    "arrow-spacing": "error",
-    "space-infix-ops": "error",
-    "space-before-blocks": ["error", { 
-      "functions": "always", 
-      "keywords": "always", 
-      "classes": "always" 
-    }], 
-    "no-multi-spaces": "error",
-    "object-curly-spacing": ["warn", "always"],
-    "semi": "error",
-    "no-console": "warn",
-    "no-multiple-empty-lines": "error",
-    "no-empty": "off",
-    "quotes": ["error", "single", { "allowTemplateLiterals": true }],
-    "import/order": ["error", {
-      "groups": ["builtin", "external", "internal", "parent", "sibling", "index"], 
-      "newlines-between": "always-and-inside-groups", 
-      "alphabetize": {
-        "order": 'asc',
-        "caseInsensitive": true
-      }, 
-      "pathGroups": [
-        {
-          "pattern": "controlers/**",
-          "group": "internal"
-        },
-        {
-          "pattern": "models/**",
-          "group": "internal"
-        },
-        {
-          "pattern": "framework/**",
-          "group": "internal"
-        },
-        {
-          "pattern": "services/**",
-          "group": "internal"
-        },
-      ],
-    }], 
-    "import/newline-after-import": ["error", { "count": 1 }]
-  }
-}
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
--- src/models/user/user.model.ts
+++
@@ -1,9 +0,0 @@
-import { IBaseModelAttached } from 'models/abstraction/base.interface';
-
-export interface IUser {
-  email: string;
-  password: string;
-  accountState: number;
-}
-
-export type IUserAttached = IUser & IBaseModelAttached;
